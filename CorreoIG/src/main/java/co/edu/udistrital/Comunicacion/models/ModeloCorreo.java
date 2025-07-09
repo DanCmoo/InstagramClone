23f4604a16d485f8fdb070551d8bbcfb959360f9
DanCmoo <dankurso845gmail.com>
2025-07-08 19:52:34 -0500

    Update CorreoIG

--- CorreoIG/src/main/java/co/edu/udistrital/Comunicacion/models/ModeloCorreo.java
+++ CorreoIG/src/main/java/co/edu/udistrital/Comunicacion/models/ModeloCorreo.java
@@ -5,10 +5,41 @@ import lombok.Data;
 
 import java.sql.Timestamp;
 
-@Data
+
 @AllArgsConstructor
 public class ModeloCorreo {
     private String email;
     private String username;
     private Timestamp timestamp;
+
+    public ModeloCorreo() {
+    }
+    public ModeloCorreo(String email, String username) {
+        this.email = email;
+        this.username = username;
+        this.timestamp = new Timestamp(System.currentTimeMillis());
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
 }
